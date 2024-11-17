961fda1585dfb6cb89f2cc710882a609a31bab9f
Maeva D <delruemaeva@orange.fr>
2024-11-17 21:58:42 +0100

    create a proper file for convertToBase64 func and use it in offer/publish

--- functions/cloudinary.js
+++ functions/cloudinary.js
@@ -1,11 +1,9 @@
 // npm i cloudinary express-fileupload
 
-const fileUpload = require("express-fileupload"); // middleware qui permet de récupérer les fichiers transmis par les clients
-
 /////////////////
 // Grâce au middleware fileUpload, on a réussi à récupérer les fichiers envoyés via postman.
 // Ils sont désormais stockés dans req.files (voir console.log()) qui contiendra la clé dont nous nous sommes servis pour envoyer les fichiers
-// maintenant, il faut renseigner ses identifiants dans la config du package :
+// Maintenant, il faut renseigner ses identifiants dans la config du package :
 const cloudinary = require("cloudinary").v2; // importer cloudinary et renseigner ses identifiants dans la config du package
 
 require("dotenv").config();
@@ -21,3 +19,10 @@ cloudinary.config({
 const convertToBase64 = (file) => {
   return `data:${file.mimetype};base64,${file.data.toString("base64")}`;
 };
+
+//// -- Comment appeler cette fonction pour convertir un fichier ?
+//// -- Le package cloudinary utilise la méthode cloudinary.uploader.upload(file, options, callback) pour envoyer un fichier.
+//// -- On devra remplacer file par notre fichier au format base64 à l'aide de la fonction plus haut. Les paramètres options et callback sont facultatifs.
+// => const pictureConverted = await cloudinary.uploader.upload(convertToBase64(file);
+
+module.exports = convertToBase64;
--- routes/offers.js
+++ routes/offers.js
@@ -2,30 +2,14 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const Offer = require("../Models/Offer");
-// Uploads de photos :
-const cloudinary = require("cloudinary").v2;
-require("dotenv").config();
 // middlewares :
 const isAuthenticated = require("../middlewares/isAuthenticated");
-const fileupload = require("express-fileupload");
+const fileupload = require("express-fileupload"); // permet de récupérer les fichiers transmis par les clients
+// Uploads de photos :
+const cloudinary = require("cloudinary").v2;
+const convertToBase64 = require("../functions/cloudinary");
 // Fonction pour gestion des erreurs :
 const handleErrorMessages = require("../functions/handleErrorMessages");
-
-/////////////////
-// Grâce au middleware fileUpload, on a réussi à récupérer les fichiers envoyés via postman.
-// Ils sont désormais stockés dans req.files (voir console.log()) qui contiendra la clé dont nous nous sommes servis pour envoyer les fichiers
-// maintenant, il faut renseigner ses identifiants dans la config du package :
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-// On ne peut pas directement envoyer un buffer à Cloudinary.
-// C'est pourquoi nous allons devoir transformer le buffer en base64, qui est un format compréhensible par Cloudinary.
-// Il faut alors créer une fonction de conversion pour cloudinary :
-const convertToBase64 = (file) => {
-  return `data:${file.mimetype};base64,${file.data.toString("base64")}`;
-};
 
 ////// ROUTES
 // Poster une annonce :
@@ -44,8 +28,6 @@ router.post(
         // Donc on utilisera une boucle for classique :
         if (picturesToUpload.length > 1) {
           for (let i = 0; i < picturesToUpload.length; i++) {
-            // Le package cloudinary utilise la méthode cloudinary.uploader.upload(file, options, callback) pour envoyer un fichier.
-            // On devra remplacer file par notre fichier au format base64. Les paramètres options et callback sont facultatifs.
             const pictureConverted = await cloudinary.uploader.upload(
               convertToBase64(picturesToUpload[i])
             );
