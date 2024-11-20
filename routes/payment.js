beb7d4417896548334d0a0a22e893829eb136f59
Maeva D <delruemaeva@orange.fr>
2024-11-20 21:38:27 +0100

    create /payment route

---
+++ routes/payment.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/payment", async (req, res) => {
+  try {
+    const { amount, currency } = req.body;
+    // On crée une intention de paiement
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: currency,
+      description: "Vinted",
+    });
+    // On renvoie les informations de l'intention de paiement au client
+    res.json(paymentIntent);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
